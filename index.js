92b86d83917e999c65946cdd1d0f185547645a99
Kevin Taylor <kevintaylor&84@gmail.com>
2022-11-10 16:27:58 -0500

    added eventlistener

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ let ballElement = document.getElementById('ball');
 let playerElement = document.getElementById('player')
 let computerElement = document.getElementById('computer')
 
-let ballPosition = {x:50, y:50}
-let playerPosition = {}
+let ballPosition = {x:400, y:400}
+let playerPosition = {x:0, y:0}
 
+
+//// eventlistener for player paddle
+
+document.addEventListener('keydown', (e)=>{
+  console.log('keydown', e.key)
+  if(e.key =='ArrowUp'){
+    playerPosition.y -= 1;
+  }
+  if(e.key == 'ArrowDown'){
+    playerPosition.y += 1;
+  }
+  playerElement.style.top = playerPosition.y + 'px'
+
+})
 // let intervalID;
 // let count = 0;
 
