1e4c41ec064802214f8e0d4fbeea1719f5544b31
ackidd <60202870+ackidd@users.noreply.github.com>
2022-11-10 14:46:59 -0600

    Created paddles

---
+++ index.js
@@ -0,0 +1,16 @@
+let ballElement = document.getElementById('ball');
+let playerElement = document.getElementById('player')
+let computerElement = document.getElementById('computer')
+
+let ballPosition = {x:50, y:50}
+let playerPosition = {}
+
+// let intervalID;
+// let count = 0;
+
+
+
+// const frame = () => {
+//   count++
+
+// }
